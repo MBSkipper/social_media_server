9f7070a51c5b29c55e564c5a75d95a84a08c4e16
MBSkipper <trawler22@gmail.com>
2026-02-16 14:00:45 +0000

    basic server folders & set up

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+console.log("ENV CHECK:", {
+  PORT: process.env.PORT,
+  MONGODB_URL: process.env.MONGODB_URL
+})
+
+//Set up routes here
+//Set up routes here
+//Set up routes here
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Route connections here
+//Route connections here
+//Route connections here
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Social Media API',
+        now: new Date()
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => console.log('Server is ready!'))
+        .catch((error) => console.log('DB connection error', error))
+})
 
 
 
+/**
+ NOTES TO CODE
+ Line 1 - imports the express library.  Express is a function that creates a server application
+ Line 2 - imports mongoose
+ Line 3 - imports dotenv
+ Line 4 - runs dotenv.config() file
+ Line 10 - const app = express() calls express to execute the  express function, it creates a new application instance and stores it in the variable named app.  this is important because everything in Express attached to app
+ Line 12 - app.use(express.json()) - adds JSON middleware
+ Line 13 - the code ..express.urlencoded(..) parses incoming data and converts it into a JavaScript object
+ Line 13 - app.use is middleware so it must run before routes so the routes can read req.body
+ Line 13 - ({ extended: false })) uses a simple parser typically for simple data rather than nested objects
+ Line 19 - '/' is the root URL, (req, res) => { } is the route handler function
+ Line 20 - res.json(...) is the response back to the client
+ Line 20 - 23 - {message: .., now: ... } is the object sent back to the client to confirm the server is running, that the routing works and JSON responses work
 
 
+ */
 
 /* DESIGN NOTES
 Social Media App Requirements
